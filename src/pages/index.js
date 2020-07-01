ca1351fd06a28932ef07509b093d219b54c4f1d5
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-07-01 15:27:13 -0300

    chore: adjust typo name

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import SEO from "../components/Seo"
+import SEO from "../components/Xama"
 
 const Home = () => {
   const { logo } = useStaticQuery(
